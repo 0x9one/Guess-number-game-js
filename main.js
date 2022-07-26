124d7cb9be20e3c76299d25b34105d4744c2a73c
SwapCode08 <sy47525@gmail.com>
2022-07-26 16:49:18 +0100

    Update style of the game

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ function checkGuess() {
     }
 
     guesses.textContent += userGuess + ' ';
+    
 
     if (userGuess === randomNumber) {
         lastResult.textContent = 'Congratulations! You got it right!';
